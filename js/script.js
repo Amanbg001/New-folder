2335d67d57e55eaf23bf8713e4d8e204364b9cbf
Aman bhargava <amanbg001@hmail.comgit config --global user.email>
2024-06-08 23:30:04 +0530

    new code for project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteElement = document.getElementById('quote');
+const authorElement = document.getElementById('author');
+const getNewQuoteButton = document.getElementById('get-new-quote');
+
+getNewQuoteButton.addEventListener('click', async () => {
+    try {
+        const response = await fetch('api/quotes.json');
+        const data = await response.json();
+        const randomQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+        quoteElement.textContent = randomQuote.text;
+        authorElement.textContent = randomQuote.author;
+    } catch (error) {
+        console.error(error);
+    }
+});
